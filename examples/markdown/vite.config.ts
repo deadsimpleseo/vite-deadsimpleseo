6f769923418c141c961932d3ad8ad051736f5dbe
= <=>
2026-01-12 04:18:23 -0500

    Add gh-pages deployment.

--- examples/markdown/vite.config.ts
+++ examples/markdown/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import deadSimpleSEO from 'vite-deadsimpleseo';
 
 export default defineConfig({
+  base: process.env.BASE_URL || '/',
   plugins: [
     react(),
     deadSimpleSEO({
