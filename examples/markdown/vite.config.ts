2e90baab0aa36948ee3873b3e938142f429e3bd5
= <=>
2026-01-12 04:32:45 -0500

    Install all dependencies.

--- examples/markdown/vite.config.ts
+++ examples/markdown/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import deadSimpleSEO from 'vite-deadsimpleseo';
 
 export default defineConfig({
-  base: process.env.BASE_URL || '/',
   plugins: [
     react(),
     deadSimpleSEO({
